995250c6d5fb8b78db219c553d7226da780f4b9b
RICO MAI <ricomai@ricos-mac.local>
2017-08-28 14:10:23 -0400

    third commit

---
+++ src/app/mock_product/mock_product.model.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by ricomai on 8/28/17.
+ */
+
+export class ProductModel{
+  id:number;
+  name:string;
+  imgUrl:string;
+}
---
+++ src/app/mock_product/mock_product.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by ricomai on 8/28/17.
+ */
+
+import { ProductModel } from './mock_product.model';
+
+export const Products : ProductModel[]= [
+  {
+    id:1 ,
+    name:'',
+    imgUrl:''
+  }
+]
